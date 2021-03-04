8240a3f8a3f26009c82e98c68a581863251d1a0b
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-03-04 16:56:40 +0100

    build: Main

---
+++ arnoldEnumType/src/main/java/edu/pingpong/arnoldEnumType/Arnold.java
@@ -0,0 +1,13 @@
+package edu.pingpong.arnoldEnumType;
+
+/**
+ * Hello world!
+ *
+ */
+public class Arnold 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
